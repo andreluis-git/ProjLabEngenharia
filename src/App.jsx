90dcf3336c259fdae36817076d84eddfcc39cc8a
Andre Rodrigues <almr.andre@gmail.com>
2021-10-11 21:23:17 -0300

    Roteamento

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,32 @@ import React from 'react';
 import Header from './components/Header'
 import SideMenu from './components/SideMenu'
 import ContentPage from './components/ContentPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import User from './components/User';
 
 import './App.css'
 
 export default function App() {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <Header/>
-            </div>
-            <div className="row">
-                <div className="col-2 p-0" style={{backgroundColor: 'red'}}>
-                    <SideMenu />
-                </div>
-                <div className="col-9">
-                    <ContentPage/>
-                </div>
-            </div>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <Header/>
+                            </div>
+                            <div className="row">
+                                <div className="col-2 p-0" style={{backgroundColor: 'red'}}>
+                                    <SideMenu />
+                                </div>
+                                <div className="col-9">
+                                    <ContentPage/>
+                                </div>
+                            </div>
+                        </div>
+                </Route>
+                <Route exact path="/user" component={User} />
+            </Switch>
+        </Router>
     );
 }
