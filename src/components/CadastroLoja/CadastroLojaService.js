31f1c067dd417d88f7fc31400af78c0677177a73
Andre Rodrigues <andre.local>
2021-10-19 22:11:55 -0300

    Gravar loja

--- src/components/CadastroLoja/CadastroLojaService.js
+++ src/components/CadastroLoja/CadastroLojaService.js
@@ -2,29 +2,30 @@ import axios from 'axios'
 
 const addPost = data => {
     let formdata = new FormData();
-    formdata.append("form", {"nome":"nomestr"})
+    let requestJson = {
+        nome: data["nome"],
+        facebook: data["facebook"],
+        instagram: data["instagram"],
+        whatsapp: data["celular"],
+        email: data["email"]
+    }
+    formdata.append("form", JSON.stringify(requestJson))
     formdata.append("imgLogo", data["imagemLogo"][0])
     formdata.append("imgBanner", data["imagemBanner"][0])
-
-    console.log(data["imagemLogo"][0])
-    console.log(data["imagemBanner"][0])
-
-    for(var key of formdata.entries()){
-        console.log(key[0] + "," + key[1])
-    }
+    
     axios({
         method: "post",
-        url: "http://localhost:8080/api/gerente/cadastrar/teste",
+        url: "http://localhost:8080/api/gerente/cadastrar/loja",
         data: formdata,
         headers: { "Content-Type": "multipart/form-data" },
     })
-.then(() => {
-    console.log("Deu tudo certo", formdata)
-    // history.push("/")
-})
-.catch(() => {
-    console.log("DEU ERRADO", formdata)
-})}
+    .then(() => {
+        console.log("Deu tudo certo", formdata)
+    })
+    .catch(() => {
+        console.log("DEU ERRADO", formdata)
+    })
+}
 
 const CadastroLojaSerice = {
     addPost
